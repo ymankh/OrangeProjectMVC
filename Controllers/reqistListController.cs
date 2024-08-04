f5c78f44171d21e499bf5e6e375339711def186d
DuhaOdat <odatduha@gmail.com>
2024-08-04 15:08:03 +0300

    debate form

---
+++ Controllers/reqistListController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrangeProjectMVC.Controllers
+{
+    public class reqistListController : Controller
+    {
+        // GET: reqistList
+        public ActionResult Index()
+
+        {
+
+            return View();
+        }
+    }
+}
